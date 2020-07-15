9f26d27ded927cc7bac09742d07de894980ff3fb
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-07-15 09:57:11 -0500

    added remote/roadey-api in UI, index.ts, roadey-get-all-users.ts, roadey-get-by-id.ts, and roadey-login.ts

---
+++ roadey-ui/src/remote/roadey-api/index.ts
@@ -0,0 +1,12 @@
+//this index is going to be for setting up the base axios client
+import axios from 'axios'
+import { lbBaseUrl } from '../../environment'
+
+// we will use this object to send off all of the other request we make to the lightlyburning api
+export const lightlyburningClient = axios.create({
+    baseURL:lbBaseUrl,
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    withCredentials:true
+})
