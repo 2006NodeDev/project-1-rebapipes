c6ff7bb710529f0ca47414a65291da9abbac9bbd
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-07-19 17:01:35 -0500

    fixed user-service.ts in services (roadey)

--- roadey/src/services/user-service.ts
+++ roadey/src/services/user-service.ts
@@ -1,27 +1,31 @@
-import { getAllUsers, getUserById, saveOneUser } from "../daos/SQL/user-dao";
+import { loginByUsernameAndPassword, getAllUsers, getUserById, saveOneUser, updateOneUser } from "../daos/SQL/user-dao";
 import { User } from "../models/User";
 import { saveProfilePicture } from "../daos/Cloud-Storage/user-images";
 import { bucketBaseUrl } from "../daos/Cloud-Storage";
 
-//the most basic service function you will see
-//all it does is call the dao
-// its easier to expand a function that already exists instead of inserting a new function in to the mix
+//Login a User
+export async function loginByUsernameAndPasswordService(username: string, password: string): Promise<User> {
+    return await loginByUsernameAndPassword(username, password)
+}
+
+//Find All Users 
 export async function getAllUsersService(): Promise<User[]> {
     return await getAllUsers()
 }
 
-
-export async function getUserByIDService(id: number): Promise<User> {
-    return await getUserById(id)
+//Find Users by Id
+export async function getUserByIdService(userId: number): Promise<User> {
+    return await getUserById(userId)
 }
 
-export async function saveOneUserService(newUser: User): Promise<User> {
+//Create New Users
+export async function saveNewUserService(newUser: User): Promise<User> {
     //two major process to manage in this function
     try {
         let base64Image = newUser.image
         let [dataType, imageBase64Data] = base64Image.split(';base64,')// gets us the two important parts of the base 64 string
-         //we need to make sure picture is in the right format
-        let contentType = dataType.split('/').pop()// split our string that looks like data:image/ext into ['data:image' , 'ext]
+        //we need to make sure picture is in the right format
+        let contentType = dataType.split('/').pop()// split our string that looks like data:image/ext into ['data:image' , 'ext']
         //then the pop method gets us the last thing in the array
         //we need to add the picture path to the user data in the sql database
         if (newUser.image) {
@@ -31,7 +35,7 @@ export async function saveOneUserService(newUser: User): Promise<User> {
         let savedUser = await saveOneUser(newUser)
 
         //we need to save a picture to cloud storage 
-       
+
         //we should probably make sure that username has no spaces in it or that we replace them with -
         await saveProfilePicture(contentType, imageBase64Data, `users/${newUser.username}/profile.${contentType}`)
         return savedUser
@@ -42,4 +46,25 @@ export async function saveOneUserService(newUser: User): Promise<User> {
 
     //if we can't save the user in the db, don't save the picture
     //if we do save the user and the picture save fails - pretend that nothing happened ( you should probably update the user to set the image to null)
+}
+
+//Update User
+export async function updateUserService(updatedUser: User): Promise<User> {
+    try {
+        let base64Image = updatedUser.image
+        let [dataType, imageBase64Data] = base64Image.split(';base64,')
+        let contentType = dataType.split('/').pop()
+
+        if (updatedUser.image) {
+            updatedUser.image = `${bucketBaseUrl}/users/${updatedUser.username}/profile.${contentType}`
+        }
+        //we need to save new user data to the sql database
+        let updatedOneUser = await updateOneUser(updatedUser)
+        await saveProfilePicture(contentType, imageBase64Data, `users/${updatedUser.username}/profile.${contentType}`)
+
+        return updatedOneUser
+    } catch (e) {
+        console.log(e);
+        throw e
+    }
 }
