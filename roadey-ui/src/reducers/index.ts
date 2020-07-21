d1fa07b01e922b8385aac09d3d00e5ead01c1e7e
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-07-20 21:00:47 -0500

    updated index.ts in reducers

--- roadey-ui/src/reducers/index.ts
+++ roadey-ui/src/reducers/index.ts
@@ -2,14 +2,8 @@
 //the really important stuff goes here
 
 import { combineReducers } from "redux";
-import { clickerReducer } from "./clicker-reducer";
 import { User } from "../models/User";
 import { loginReducer } from "./login-reducer";
-
-//interface for clicker
-export interface IClickerState{
-    clicks:number
-}
 
 //interface for login
 export interface ILoginState{
@@ -21,13 +15,11 @@ export interface ILoginState{
 
 //the type definition for our entire state object
 export interface IState{//the data that will make up our application
-    clickerState:IClickerState
     loginState:ILoginState
 }
 
 //this will be the total state of our redux store
 export const state = combineReducers<IState>({
     //all of the sub reducer functions go in here
-    clickerState:clickerReducer,
     loginState:loginReducer
 })
