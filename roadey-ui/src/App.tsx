be64616a602d9ca847a56dc8e39a1e36d972a31b
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-07-21 09:05:44 -0500

    updated css and app.tsx

--- roadey-ui/src/App.tsx
+++ roadey-ui/src/App.tsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
-import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
-//import { FirstComponent } from './components/FirstComponent/FirstComponent';
-//import { TitleComponent } from './components/TitleComponent/TitleComponent';
-//import { FancyBorder } from './components/FancyBorderComponent/FancyBorderComponent';
+import { TitleComponent } from './components/TitleComponent/TitleComponent';
 import { NavBarComponent } from './components/NavBarComponent/NavBarComponent';
 import { LoginComponent } from './components/LoginComponent/LoginComponent';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { User } from './models/User';
-//import { UserDisplayComponent } from './components/UserDisplayComponent/UserDisplay';
+import { UserDisplayComponent } from './components/UserDisplayComponent/UserDisplay';
 import { ProfileComponent } from './components/ProfileComponent/ProfileComponent';
 import { AllUsersComponent } from './components/AllUserComponent/AllUsersComponent';
 import { NewUserComponent } from './components/NewUserComponent/NewUserComponent';
 import {ToastContainer} from 'react-toastify';
 import { makeStyles } from '@material-ui/core';
+import './App.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,7 +27,7 @@ function App() {
   const [currentUser, changeCurrentUser] = useState<null | User>(null)
   const classes = useStyles()
   return (
-    <div className={classes.root} style={ {border: '3px solid #ff5722' } }>
+    <div  className="container">
       <Router>
         <header><NavBarComponent user={currentUser}/></header>
         {/* <NavBarComponent user={currentUser}/> */}
@@ -47,3 +45,6 @@ function App() {
 }
 
 export default App;
+
+
+//<div className={classes.root} style={ {border: '3px solid #ff5722' } }>
